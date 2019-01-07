cba2ca48e0d220c5a0ba09b81ad5155f296e292a
sparsick <sparsick@web.de>
2019-01-07 15:03:25 +0100

    enably buggy use case

--- src/main/java/com/github/sparsick/swagger/playground/TestClient.java
+++
@@ -1,9 +0,0 @@
-package com.github.sparsick.swagger.playground;
-
-import io.swagger.client.ApiClient;
-import org.springframework.web.client.RestTemplate;
-
-public class TestClient {
-
-    private ApiClient apiClient = new ApiClient(new RestTemplate());
-}
